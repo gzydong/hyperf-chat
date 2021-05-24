dabf8b39c926bc06b0197b89a8d27d8a1e313db5
gzydong <837215079@qq.com>
2021-05-24 12:42:53 +0800

    优化代码

--- app/Controller/Api/V1/AuthController.php
+++ app/Controller/Api/V1/AuthController.php
@@ -204,15 +204,4 @@ class AuthController extends CController
 
         return $this->response->success($data, '验证码发送成功...');
     }
-
-    /**
-     * 发送验证码
-     * @RequestMapping(path="test", methods="get")
-     */
-    public function test()
-    {
-        //$user = User::first();
-        //$name = $user->yuanodng;
-        var_dump($name);
-    }
 }
