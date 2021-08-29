649a90ca0e0a5e08c698c52e5f387fb6bb09e3c2
gzydong <837215079@qq.com>
2021-08-29 22:12:50 +0800

    添加单元测试

--- app/Helper/ArrayHelper.php
+++ app/Helper/ArrayHelper.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Helper;
 
-
 class ArrayHelper
 {
     /**
---
+++ test/Cases/Helper/ArrayHelperTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace HyperfTest\Cases\Helper;
+
+
+use App\Helper\ArrayHelper;
+use HyperfTest\HttpTestCase;
+
+class ArrayHelperTest extends HttpTestCase
+{
+    public function testIsRelationArray()
+    {
+        $this->assertTrue(!ArrayHelper::isRelationArray([1, 2, 3, 4, 5]), '判断是否是关联数组失败1！');
+        $this->assertTrue(ArrayHelper::isRelationArray([1, 6 => 2, 3, 4, 5]), '判断是否是关联数组失败2！');
+        $this->assertTrue(ArrayHelper::isRelationArray(['k1' => 'test', 'k2' => 'test2']), '判断是否是关联数组失败3！');
+    }
+}
